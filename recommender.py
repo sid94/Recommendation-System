1ad2c10a2a1753e97d9249af5e2342da3117b579
SidKolhapure <sid.kolhapure94@gmail.com>
2020-11-03 22:01:29 -0500

    complete the code

--- recommender.py
+++ recommender.py
@@ -1,27 +1,50 @@
 # import numpy as np
 import os
+import numpy as np
 from pearson import Pearson
+from sklearn.model_selection import train_test_split
+import pickle
+import math
 
 class Recommendation:
     def __init__(self,users,items):
         self.users = users + 1
         self.items = items + 1
+        self.input = np.array([[0,0,0]])
         self.matrix = [[0 for _ in range(self.items)] for _ in range(self.users)]
         self.similarityMat = [[]]
         self.out = [[0 for _ in range(self.items)] for _ in range(self.users)]
+        self.accFlag = True
+        self.test = None
         
     def readFile(self):
-        if(os.path.isfile("dataset/train.txt")):
-            with open("dataset/train.txt", "r") as file:
-                for line in file:
-                    user,item,rating = line.split()
-                    self.matrix[int(user)][int(item)] = int(rating)
-                file.close()
+        
+        if(not self.accFlag):
+            if(os.path.isfile("dataset/train.txt")):
+                with open("dataset/train.txt", "r") as file:
+                    for line in file:
+                        user,item,rating = line.split()
+                        self.matrix[int(user)][int(item)] = int(rating)
+                    file.close()
+        else:
+            self.inputToNpArray()
+            self.input = np.delete(self.input, (0), axis=0)
+            np.random.shuffle(self.input)
+            x_train ,x_test = train_test_split(self.input,test_size=0.2)
+            # print(len(x_train))
+            # print(len(x_test))
+            self.test = x_test
+            
+            for i,j,rate in x_train:
+                self.matrix[int(i)][int(j)] = int(rate)
         # self.printRowByRow(self.matrix)
                 
     def similarity(self):
         objPearson = Pearson(self.users,self.matrix)
         self.similarityMat = objPearson.similarity()
+        with open("simi.pkl", "wb") as file:
+            pickle.dump(self.similarityMat,file)
+            file.close()
         # self.printRowByRow(self.similarityMat)
         
     def printRowByRow(self,matrix):
@@ -42,6 +65,8 @@ class Recommendation:
         
     def predict(self):
         print("prediction start")
+        file = open('simi.pkl', 'rb')
+        self.similarityMat = pickle.load(file)
         for i in range(1,self.users):
             mean_i = self.mean(self.matrix[i])
             for j in range(1,self.items):
@@ -62,9 +87,39 @@ class Recommendation:
                 else:
                     self.out[i][j] = self.matrix[i][j]
                     print((i,j))
-                
-                
+    
+    def write(self):
+        with open("output.txt", "w+") as file:
+            for i in range(1,len(self.out)):
+                for j in range(1,len(self.out[0])):
+                    file.write("{} {} {}\n".format(i,j,self.out[i][j]))
+            file.close()
+        
         # self.printRowByRow(self.out)
+        
+    def accuracy(self):
+        total = 0
+        N = len(self.test)
+        diffSqrSum = 0
+        for i,j,rate in self.test:
+            print(int(self.out[i][j]), "   " ,int(rate))
+            diffSqrSum += (int(rate) -  int(self.out[i][j]))**2
+            if(int(self.out[i][j]) == int(rate)):
+                total += 1
+        
+        print("RMSE = ", math.sqrt(diffSqrSum/N))
+        print("accuracy = ", (total/len(self.test)) * 100 )
+                
+    def inputToNpArray(self):
+        if(os.path.isfile("dataset/train.txt")):
+            with open("dataset/train.txt", "r") as file:
+                for line in file:
+                    user,item,rating = line.split()
+                    arr = [int(user),int(item),int(rating)]
+                    self.input = np.append(self.input,[arr],axis=0)
+                file.close()
+        
+        
         
 if __name__ == '__main__':
     
@@ -80,4 +135,6 @@ if __name__ == '__main__':
     objRec.readFile()
     objRec.similarity()
     objRec.predict()
+    objRec.accuracy()
+    objRec.write()
     # objRec.predict()
